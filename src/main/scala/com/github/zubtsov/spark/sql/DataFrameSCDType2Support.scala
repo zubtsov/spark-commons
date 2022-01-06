cd3553e2791ffa6ae4e5a9532cbb085c8cfef625
Ruslan_Zubtsov <Ruslan_Zubtsov@epam.com>
2022-01-06 22:11:11 +0300

    - added test case for SCD2 conversion

--- src/main/scala/com/github/zubtsov/spark/sql/DataFrameSCDType2Support.scala
+++ src/main/scala/com/github/zubtsov/spark/sql/DataFrameSCDType2Support.scala
@@ -1,28 +1,57 @@
 package com.github.zubtsov.spark.sql
 
-import com.github.zubtsov.spark.enums.JoinType
-import com.github.zubtsov.spark.enums.JoinType.JoinType
-import com.github.zubtsov.spark.defaultCaseSensitivity
-import org.apache.spark.sql.{Column, DataFrame}
-//TODO: implement
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.functions._
+
+//TODO: add joins?
+//TODO: support soft delete expression
+//TODO: add compact changes method? (consider gaps)
 object DataFrameSCDType2Support {
-  implicit class DataFrameSCDType2Support(df: DataFrame) {
-    private def joinWithSCD2(other: DataFrame, usingColumns: Seq[String], joinType: JoinType = JoinType.Inner): DataFrame = {
-      ???
-    }
 
-    private def joinWithSCD2(other: DataFrame, joinExpr: Column, joinType: JoinType): DataFrame = {
-      ???
-    }
+  object implicits {
 
-    private def convert(primaryKeyColNames: Seq[String],
-                scdType2ColNames: Seq[String], //all other columns considered as type 1
-                startDateColName: String = "start_date",
-                endDateColName: String = "end_date",
-                pkColNamesCaseSensitive: Boolean = defaultCaseSensitivity,
-                stringAttrsCaseSensitive: Boolean = defaultCaseSensitivity,
-                trimStringType2Cols: Boolean = false): DataFrame = {
-      ???
+    implicit class DataFrameSCDType2SupportImpl(df: DataFrame) {
+      /**
+       * Converts periodic snapshot table to SCD type 2 + type 1 table and merges time periods/intervals where no SCD type 2 attributes changed.
+       * Active records can be found using end_date = null condition.
+       *
+       * @param primaryKeyColNames
+       * @param scdType2ColNames
+       * @param businessDateColName essentially effective/start date of a row
+       * @param startDateColName
+       * @param endDateColName
+       * @return
+       */
+      def periodicSnapshotToSCD2(primaryKeyColNames: Seq[String],
+                                 scdType2ColNames: Seq[String], //all other columns considered as type 1
+                                 businessDateColName: String,
+                                 startDateColName: String = "start_date",
+                                 endDateColName: String = "end_date"): DataFrame = {
+        val groupedSCD2RowIndex = "tmp_grouped_scd2_row_index_"
+        val previousRowSCD2AttrsAreDifferent = "_tmp_prev_row_scd2_attrs_diff_"
+        val lastRowOfSCD2Group = "_tmp_last_row_of_scd2_group_"
+
+        val w = Window.partitionBy(primaryKeyColNames.head, primaryKeyColNames.tail: _*)
+          .orderBy(startDateColName)
+        val w2 = Window.partitionBy(primaryKeyColNames.head, primaryKeyColNames.tail: _*)
+          .orderBy(startDateColName)
+          .rowsBetween(Window.unboundedPreceding, Window.currentRow)
+        val w3 = Window.partitionBy(groupedSCD2RowIndex, primaryKeyColNames: _*)
+
+        df
+          .withColumnRenamed(businessDateColName, startDateColName)
+          .withColumn(endDateColName, lead(startDateColName, 1).over(w))
+          .withColumn(previousRowSCD2AttrsAreDifferent, when(scdType2ColNames.map(cn => {
+            col(cn) =!= lag(cn, 1).over(w) or lag(cn, 1).over(w).isNull
+          }).reduce(_ or _), lit(1)).otherwise(lit(0))
+          )
+          .withColumn(groupedSCD2RowIndex, sum(previousRowSCD2AttrsAreDifferent).over(w2))
+          .withColumn(lastRowOfSCD2Group, lead(groupedSCD2RowIndex, 1).over(w) =!= col(groupedSCD2RowIndex) or lead(groupedSCD2RowIndex, 1).over(w).isNull)
+          .withColumn(startDateColName, min(startDateColName).over(w3))
+          .where(col(lastRowOfSCD2Group))
+          .drop(previousRowSCD2AttrsAreDifferent, groupedSCD2RowIndex, lastRowOfSCD2Group)
+      }
     }
   }
 }
---
+++ src/test/scala/com/github/zubtsov/spark/sql/DataFrameSCDType2SupportTest.scala
@@ -0,0 +1,46 @@
+package com.github.zubtsov.spark.sql
+
+import com.github.zubtsov.spark.SparkFunSuite
+import com.github.zubtsov.spark.Literals._
+import org.apache.spark.sql.Row
+import com.github.zubtsov.spark.DataFrameComparison._
+
+class DataFrameSCDType2SupportTest extends SparkFunSuite {
+  test("Test SCD type 2 conversion") {
+    import com.github.zubtsov.spark.SparkSessionCommons.implicits._
+
+    val source = spark.createDataFrame(
+      "`my_primary_key` STRING, `int_value` INT, `string_value` STRING, type1_attr STRING, `business_date` DATE",
+      Row(               "pk1",               1,                  "v1",               "1",        d"2022-01-03"),
+      Row(               "pk1",               1,                  "v1",               "1",        d"2022-01-04"),
+      Row(               "pk1",               1,                  "v1",               "1",        d"2022-01-05"),
+      Row(               "pk1",               1,                  "v1",               "1",        d"2022-01-06"),
+      Row(               "pk1",               1,                  "v1",               "2",        d"2022-01-07"),
+      Row(               "pk1",               2,                  "v1",               "3",        d"2022-01-08"),
+      Row(               "pk1",               2,                  "v1",               "4",        d"2022-01-09"),
+      Row(               "pk1",               2,                  "v2",               "5",        d"2022-01-10"),
+      Row(               "pk1",               2,                  "v2",               "6",        d"2022-01-11"),
+      Row(               "pk2",               3,                  "v3",               "7",        d"2022-01-06"),
+      Row(               "pk2",               3,                  "v3",               "8",        d"2022-01-07"),
+      Row(               "pk2",               4,                  "v3",               "9",        d"2022-01-08"),
+      Row(               "pk2",               4,                  "v3",              "10",        d"2022-01-09"),
+      Row(               "pk2",               4,                  "v4",              "11",        d"2022-01-10"),
+      Row(               "pk2",               4,                  "v4",              "12",        d"2022-01-11")
+    )
+
+    val expected = spark.createDataFrame(
+      "`my_primary_key` STRING, `int_value` INT, `string_value` STRING, type1_attr STRING, `start_date` DATE, `end_date` DATE",
+      Row(               "pk1",               1,                  "v1",               "2",     d"2022-01-03",   d"2022-01-08"),
+      Row(               "pk1",               2,                  "v1",               "4",     d"2022-01-08",   d"2022-01-10"),
+      Row(               "pk1",               2,                  "v2",               "6",     d"2022-01-10",            null),
+      Row(               "pk2",               3,                  "v3",               "8",     d"2022-01-06",   d"2022-01-08"),
+      Row(               "pk2",               4,                  "v3",              "10",     d"2022-01-08",   d"2022-01-10"),
+      Row(               "pk2",               4,                  "v4",              "12",     d"2022-01-10",            null)
+    )
+
+    import DataFrameSCDType2Support.implicits._
+    val actual = source.periodicSnapshotToSCD2(Seq("my_primary_key"), Seq("int_value", "string_value"), "business_date")
+
+    assertEquals(actual, expected)
+  }
+}
