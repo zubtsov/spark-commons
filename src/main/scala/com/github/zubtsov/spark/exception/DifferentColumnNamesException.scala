e4803e17b866ae64d291a9b4afc9bb965d4c5cb9
Ruslan_Zubtsov <Ruslan_Zubtsov@epam.com>
2022-01-10 15:47:05 +0300

    - fixed separators in exception classes

--- src/main/scala/com/github/zubtsov/spark/exception/DifferentColumnNamesException.scala
+++ src/main/scala/com/github/zubtsov/spark/exception/DifferentColumnNamesException.scala
@@ -3,6 +3,6 @@ package com.github.zubtsov.spark.exception
 import com.github.zubtsov.spark.DataFrameComparison.ColumnNamesDifference
 
 final case class DifferentColumnNamesException(columnNamesDifference: ColumnNamesDifference) extends Exception {
-  override def getMessage: String = s"\nMissing columns from the left ${columnNamesDifference.leftMissingCols.mkString}" +
-    s"\nMissing columns from the right ${columnNamesDifference.rightMissingCols.mkString}"
+  override def getMessage: String = s"\nMissing columns from the left ${columnNamesDifference.leftMissingCols.mkString(", ")}" +
+    s"\nMissing columns from the right ${columnNamesDifference.rightMissingCols.mkString(", ")}"
 }
--- src/main/scala/com/github/zubtsov/spark/exception/DifferentSchemasException.scala
+++ src/main/scala/com/github/zubtsov/spark/exception/DifferentSchemasException.scala
@@ -3,6 +3,6 @@ package com.github.zubtsov.spark.exception
 import com.github.zubtsov.spark.DataFrameComparison.SchemaDifference
 
 final case class DifferentSchemasException(schemaDifference: SchemaDifference) extends Exception {
-  override def getMessage: String = s"\nMissing fields from the left ${schemaDifference.leftMissingFields.mkString}" +
-    s"\nMissing fields from the right ${schemaDifference.rightMissingFields.mkString}"
+  override def getMessage: String = s"\nMissing fields from the left ${schemaDifference.leftMissingFields.mkString(", ")}" +
+    s"\nMissing fields from the right ${schemaDifference.rightMissingFields.mkString(", ")}"
 }
