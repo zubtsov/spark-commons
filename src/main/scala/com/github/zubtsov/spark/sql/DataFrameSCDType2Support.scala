03f01fe96dbd422323708001494bf8cf19bdfdf2
Ruslan_Zubtsov <Ruslan_Zubtsov@epam.com>
2022-01-10 15:39:48 +0300

    - added API method for SCD2 conversion

--- src/main/scala/com/github/zubtsov/spark/sql/DataFrameSCDType2Support.scala
+++ src/main/scala/com/github/zubtsov/spark/sql/DataFrameSCDType2Support.scala
@@ -12,46 +12,59 @@ object DataFrameSCDType2Support {
   object implicits {
 
     implicit class DataFrameSCDType2SupportImpl(df: DataFrame) {
+
       /**
-       * Converts periodic snapshot table to SCD type 2 + type 1 table and merges time periods/intervals where no SCD type 2 attributes changed.
-       * Active records can be found using end_date = null condition.
-       *
-       * @param primaryKeyColNames
-       * @param scdType2ColNames
-       * @param businessDateColName essentially effective/start date of a row
-       * @param startDateColName
-       * @param endDateColName
-       * @return
+       * [[DataFrameSCDType2Support.periodicSnapshotToSCD2]]
        */
       def periodicSnapshotToSCD2(primaryKeyColNames: Seq[String],
                                  scdType2ColNames: Seq[String], //all other columns considered as type 1
                                  businessDateColName: String,
                                  startDateColName: String = "start_date",
                                  endDateColName: String = "end_date"): DataFrame = {
-        val groupedSCD2RowIndex = "tmp_grouped_scd2_row_index_"
-        val previousRowSCD2AttrsAreDifferent = "_tmp_prev_row_scd2_attrs_diff_"
-        val lastRowOfSCD2Group = "_tmp_last_row_of_scd2_group_"
-
-        val w = Window.partitionBy(primaryKeyColNames.head, primaryKeyColNames.tail: _*)
-          .orderBy(startDateColName)
-        val w2 = Window.partitionBy(primaryKeyColNames.head, primaryKeyColNames.tail: _*)
-          .orderBy(startDateColName)
-          .rowsBetween(Window.unboundedPreceding, Window.currentRow)
-        val w3 = Window.partitionBy(groupedSCD2RowIndex, primaryKeyColNames: _*)
-
-        df
-          .withColumnRenamed(businessDateColName, startDateColName)
-          .withColumn(endDateColName, lead(startDateColName, 1).over(w))
-          .withColumn(previousRowSCD2AttrsAreDifferent, when(scdType2ColNames.map(cn => {
-            col(cn) =!= lag(cn, 1).over(w) or lag(cn, 1).over(w).isNull
-          }).reduce(_ or _), lit(1)).otherwise(lit(0))
-          )
-          .withColumn(groupedSCD2RowIndex, sum(previousRowSCD2AttrsAreDifferent).over(w2))
-          .withColumn(lastRowOfSCD2Group, lead(groupedSCD2RowIndex, 1).over(w) =!= col(groupedSCD2RowIndex) or lead(groupedSCD2RowIndex, 1).over(w).isNull)
-          .withColumn(startDateColName, min(startDateColName).over(w3))
-          .where(col(lastRowOfSCD2Group))
-          .drop(previousRowSCD2AttrsAreDifferent, groupedSCD2RowIndex, lastRowOfSCD2Group)
+        df.transform(DataFrameSCDType2Support.periodicSnapshotToSCD2(primaryKeyColNames, scdType2ColNames, businessDateColName, startDateColName, endDateColName))
       }
     }
+  }
+
+  /**
+   * Converts periodic snapshot table to SCD type 2 + type 1 table and merges time periods/intervals where no SCD type 2 attributes changed.
+   * Active records can be found using end_date = null condition.
+   *
+   * @param primaryKeyColNames
+   * @param scdType2ColNames
+   * @param businessDateColName essentially effective/start date of a row
+   * @param startDateColName
+   * @param endDateColName
+   * @return
+   */
+  def periodicSnapshotToSCD2(primaryKeyColNames: Seq[String],
+                             scdType2ColNames: Seq[String], //all other columns considered as type 1
+                             businessDateColName: String,
+                             startDateColName: String = "start_date",
+                             endDateColName: String = "end_date")
+                            (df: DataFrame): DataFrame = {
+    val groupedSCD2RowIndex = "tmp_grouped_scd2_row_index_"
+    val previousRowSCD2AttrsAreDifferent = "_tmp_prev_row_scd2_attrs_diff_"
+    val lastRowOfSCD2Group = "_tmp_last_row_of_scd2_group_"
+
+    val w = Window.partitionBy(primaryKeyColNames.head, primaryKeyColNames.tail: _*)
+      .orderBy(startDateColName)
+    val w2 = Window.partitionBy(primaryKeyColNames.head, primaryKeyColNames.tail: _*)
+      .orderBy(startDateColName)
+      .rowsBetween(Window.unboundedPreceding, Window.currentRow)
+    val w3 = Window.partitionBy(groupedSCD2RowIndex, primaryKeyColNames: _*)
+
+    df
+      .withColumnRenamed(businessDateColName, startDateColName)
+      .withColumn(endDateColName, lead(startDateColName, 1).over(w))
+      .withColumn(previousRowSCD2AttrsAreDifferent, when(scdType2ColNames.map(cn => {
+        col(cn) =!= lag(cn, 1).over(w) or lag(cn, 1).over(w).isNull
+      }).reduce(_ or _), lit(1)).otherwise(lit(0))
+      )
+      .withColumn(groupedSCD2RowIndex, sum(previousRowSCD2AttrsAreDifferent).over(w2))
+      .withColumn(lastRowOfSCD2Group, lead(groupedSCD2RowIndex, 1).over(w) =!= col(groupedSCD2RowIndex) or lead(groupedSCD2RowIndex, 1).over(w).isNull)
+      .withColumn(startDateColName, min(startDateColName).over(w3))
+      .where(col(lastRowOfSCD2Group))
+      .drop(previousRowSCD2AttrsAreDifferent, groupedSCD2RowIndex, lastRowOfSCD2Group)
   }
 }
