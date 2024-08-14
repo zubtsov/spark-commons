e5097f6b77ae6764d69c8e67d05d38923f4566b0
Ruslan Zubtsov <zubtsov.r@gmail.com>
2024-08-14 20:42:02 +0400

    - parallelized all tests

--- src/test/scala/com/github/zubtsov/spark/SparkFunSuite.scala
+++ src/test/scala/com/github/zubtsov/spark/SparkFunSuite.scala
@@ -2,11 +2,13 @@ package com.github.zubtsov.spark
 
 import org.apache.spark.sql.SparkSession
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.ParallelTestExecution
 
 //todo: share it as a part of a library
-class SparkFunSuite extends AnyFunSuite {
+class SparkFunSuite extends AnyFunSuite with ParallelTestExecution {
   protected val ss = SparkSession
     .builder()
+    .config("spark.sql.shuffle.partitions", 1)
     .config("spark.driver.host", "localhost")
     .master("local[*]")
 
